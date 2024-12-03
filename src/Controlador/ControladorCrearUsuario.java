75c20ea2181fab8c4c5509caaca695e9de2760f3
Anthony ramos roncal <codeartprogrammer@gmail.com>
2024-12-02 21:59:20 -0500

    Crear usuario validar campos

--- src/Controlador/ControladorCrearUsuario.java
+++ src/Controlador/ControladorCrearUsuario.java
@@ -5,6 +5,8 @@ import Modelo.Modelo_Login;
 import Vista.LoginVista;
 import Vista.RegistroUsuarioVista;
 
+import javax.swing.*;
+
 
 public class ControladorCrearUsuario {
     RegistroUsuarioVista vista  = new RegistroUsuarioVista();
@@ -14,13 +16,17 @@ public class ControladorCrearUsuario {
     }
     public void Iniciar() {
         vista.getBtncrear().addActionListener(_ -> {
-            Modelo_CrearUsuario modelo = new Modelo_CrearUsuario(vista);
-            modelo.RegistrarUsuario();
-            LoginVista login = new LoginVista();
-            Modelo_Login mod = new Modelo_Login();
-            new FrmLoginUsuario_Controlador(login , mod);
-            login.setVisible(true);
-            vista.dispose();
+            if(vista.getTxtcontra().getText().equals("  Ingrese contraseña") || vista.getTxtnombre().getText().equals("  Ingrese Nombre y apellido") || vista.getTxttelefono().getText().equals("   Ingrese Numero de Telefono") || vista.getTxtusuario().getText().equals("  Ingrese usuario")) {
+                JOptionPane.showMessageDialog(null, "Debe llenar todos los campos requeridos");
+            }else{
+                Modelo_CrearUsuario modelo = new Modelo_CrearUsuario(vista);
+                modelo.RegistrarUsuario();
+                LoginVista login = new LoginVista();
+                Modelo_Login mod = new Modelo_Login();
+                new FrmLoginUsuario_Controlador(login, mod);
+                login.setVisible(true);
+                vista.dispose();
+            }
         });
     }
 }
